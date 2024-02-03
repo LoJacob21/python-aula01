369daf2cec05d1c805132152ffa2cd03e4ed1840
Lorena Jacob <lorenap_rodrigues@hotmail.com>
2024-02-03 10:03:34 -0300

    Projetos aula 01

---
+++ imc.py
@@ -0,0 +1,4 @@
+peso = float(input("Informe seu peso: "))
+altura = float(input("Informe sua altura: "))
+
+print(peso / (altura*altura))
---
+++ main.py
@@ -0,0 +1,11 @@
+# print ("Hello World!")
+
+# nome = input ("Qual o seu nome?")
+
+# print("Seja bem vindo a programação", nome)
+# print(f"Você sabe a origem do nome {nome}?")
+
+num1 = float(input("Número 1: "))
+num2 = float(input("Número 2: "))
+
+print (num1 + num2)
